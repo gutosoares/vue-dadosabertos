54a980a737bf41d2a69409242f689331aeee2ba9
Augusto Soares <gutosoares@outlook.com>
2017-05-26 21:01:36 -0300

    Search commission function is done

--- src/components/Comissao.vue
+++ src/components/Comissao.vue
@@ -30,11 +30,11 @@
       <div class="col s12 m12">
         <div class="card">
           <div class="card-content">
-            <span class="card-title">Busque um comissão em um determinado périodo de tempo</span>
+            <span class="card-title">Busque uma comissão em um determinado périodo de tempo</span>
             <div class="row">
               <div class="col s6">
                 <label>Selecione Comissão</label>
-                <select class="browser-default" v-model="searchProperties.commissionValue">
+                <select class="browser-default" v-model="searchProperties.commissionId">
                   <option value="" disabled selected>Selecione um opção</option>
                   <option v-for="commission in listOfCommission" :value=commission.id>{{ commission.nome }}</option>
                 </select>
@@ -54,10 +54,13 @@
                 </select>
               </div>
             </div>
-            <a class="waves-effect waves-light btn" type="submit">Buscar</a>
+            <a class="waves-effect waves-light btn" type="submit" @click.prevent="searchCommission()">Buscar</a>
           </div>
         </div>
       </div>
+    </div>
+    <div class="row">
+
     </div>
   </div>
 </template>
@@ -68,7 +71,7 @@ export default {
   data () {
     return {
       searchProperties: {
-        commissionValue: '',
+        commissionId: '',
         yearValue: '',
         monthValue: ''
       },
@@ -84,7 +87,7 @@ export default {
         { "id": 8, "nome": "Direitos Humanos" }
       ],
       years: [
-        { "value": 2010 }, { "value": 2011 }, { "value": 2012 }, { "value": 2013 }, { "value": 2014 }, { "value": 2015 }, { "value": 2016 }, { "value": 2017 },
+        { "value": 2010 }, { "value": 2011 }, { "value": 2012 }, { "value": 2013 }, { "value": 2014 }, { "value": 2015 }, { "value": 2016 }, { "value": 2017 }, { "value": 2003 }
       ],
       months: [
         { "value": 1 }, { "value": 2 }, { "value": 3 }, { "value": 4 }, { "value": 5 }, { "value": 6 }, { "value": 7 }, { "value": 8 }, { "value": 9 }, { "value": 10 }, { "value": 11 }, { "value": 12 }
@@ -95,6 +98,11 @@ export default {
     fetchTypeOfCommissionType() {
       this.$http.get('http://dadosabertos.almg.gov.br/ws/comissoes/tipos?formato=json').then(response => {
         this.listOfCommissionType = response.data.list
+      })
+    },
+    searchCommission() {
+      this.$http.get('http://dadosabertos.almg.gov.br/ws/comissoes/'+ this.searchProperties.commissionId +'/composicoes/'+ this.searchProperties.yearValue +'/'+ this.searchProperties.monthValue + '?formato=json').then(response => {
+        console.log(response)
       })
     }
   },
