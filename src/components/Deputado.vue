242ca897a7b900d5489d56fcc5aa2e2da78ce74b
Augusto Soares <gutosoares@outlook.com>
2017-05-28 11:10:50 -0300

    Deputy info done

--- src/components/Deputado.vue
+++ src/components/Deputado.vue
@@ -1,22 +1,70 @@
 <template>
   <div class="deputado">
     <h1>Deputados</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="row">
+      <div class="col s6">
+        <div class="card">
+          <div class="card-content">
+            <span class="card-title">O Papel dos Deputados</span>
+            <p>O Poder Legislativo cumpre papel imprescindível perante a sociedade do País, visto que desempenha três funções primordiais para a consolidação da democracia: representar o povo brasileiro, legislar sobre os assuntos de interesse nacional e fiscalizar a aplicação dos recursos públicos.</p>
+            <br>
+            <p>Nesse contexto, a Câmara dos Deputados, autêntica representante do povo brasileiro, exerce atividades que viabilizam a realização dos anseios da população, mediante discussão e aprovação de propostas referentes às áreas econômicas e sociais, como educação, saúde, transporte, habitação, entre outras, sem descuidar do correto emprego, pelos Poderes da União, dos recursos arrecadados da população com o pagamento de tributos.</p>
+          </div>
+          <div class="card-action">
+            <a href="http://www2.camara.leg.br/a-camara/conheca" target="_blank">Saiba mais aqui</a>
+          </div>
+        </div>
+      </div>
+      <div class="col s6">
+        <div class="card">
+          <div class="card-content">
+            <span class="card-title">Número de Deputados por Estado</span>
+              <div class="row">
+                <div class="col s4">
+                  <ul>
+                    <li><div>Acre<span class="secondary-content right">8</span></div></li>
+                    <li><div>Alagoas<span class="secondary-content right">9</span></div></li>
+                    <li><div>Amazonas<span class="secondary-content right">8</span></div></li>
+                    <li><div>Amapá<span class="secondary-content right">8</span></div></li>
+                    <li><div>Bahia<span class="secondary-content right">39</span></div></li>
+                    <li><div>Ceará<span class="secondary-content right">22</span></div></li>
+                    <li><div>Distrito Federal<span class="secondary-content right">8</span></div></li>
+                    <li><div>Espírito Santo<span class="secondary-content right">10</span></div></li>
+                    <li><div>Goiás<span class="secondary-content right">17</span></div></li>
+                  </ul>
+                </div>
+                <div class="col s4">
+                  <ul>
+                    <li><div>Maranhão<span class="secondary-content right">18</span></div></li>
+                    <li><div>Minas Gerais<span class="secondary-content right">53</span></div></li>
+                    <li><div>Mato Grosso do Sul<span class="secondary-content right">8</span></div></li>
+                    <li><div>Mato Grosso<span class="secondary-content right">8</span></div></li>
+                    <li><div>Pará<span class="secondary-content right">8</span></div></li>
+                    <li><div>Paraíba<span class="secondary-content right">12</span></div></li>
+                    <li><div>Pernambuco<span class="secondary-content right">25</span></div></li>
+                    <li><div>Piauí<span class="secondary-content right">10</span></div></li>
+                    <li><div>Paraná<span class="secondary-content right">30</span></div></li>
+                  </ul>
+                </div>
+                <div class="col s4">
+                  <ul>
+                    <li><div>Rio de Janeiro<span class="secondary-content right">46</span></div></li>
+                    <li><div>Rio Grande do Norte<span class="secondary-content right">8</span></div></li>
+                    <li><div>Rondônia<span class="secondary-content right">8</span></div></li>
+                    <li><div>Roraima<span class="secondary-content right">8</span></div></li>
+                    <li><div>Rio Grande do Sul<span class="secondary-content right">31</span></div></li>
+                    <li><div>Santa Catarina<span class="secondary-content right">16</span></div></li>
+                    <li><div>Sergipe<span class="secondary-content right">8</span></div></li>
+                    <li><div>São Paulo<span class="secondary-content right">70</span></div></li>
+                    <li><div>Tocantins<span class="secondary-content right">8</span></div></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,7 +73,12 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      sit: [
+        { "value": 1, "name": 'Em Exercício' },
+        { "value": 2, "name": 'Exerceu Mandato' },
+        { "value": 3, "name": 'Renunciou' },
+        { "value": 4, "name": 'Perdeu Mandato' },
+      ]
     }
   }
 }
@@ -33,21 +86,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+ul li {
+  padding-bottom: 12px;
 }
 </style>
