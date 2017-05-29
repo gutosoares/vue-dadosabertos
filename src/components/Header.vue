cccad8f30619e6cc4c4fd9528bea015a6f49454b
Augusto Soares <gutosoares@outlook.com>
2017-05-28 22:08:36 -0300

    Turn header side bar responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,16 +3,18 @@
     <div class="navbar-fixed">
       <nav>
         <div class="nav-wrapper container">
-          <router-link to="/comissao" class="brand-logo center"><i class="material-icons">dashboard</i> Dados Abertos</router-link>
-          <a href="#" data-activates="menu-mobile" class="button-collapse right"><i class="material-icons">menu</i></a>
+          <router-link to="/comissao" class="brand-logo center hide-on-med-and-down"><i class="material-icons">dashboard</i> Dados Abertos</router-link>
           <ul class="right hide-on-med-and-down">
             <li class="active"><router-link to="/comissao">Comissão</router-link></li>
             <li><router-link to="/deputado">Deputados</router-link></li>
           </ul>
-          <ul class="side-nav" id="menu-mobile">
-            <li><router-link to="/comissao">Comissão</router-link></li>
-            <li><router-link to="/deputado">Deputados</router-link></li>
-          </ul>
+          <div class="menu-mobile hide-on-large-only">
+            <router-link to="/comissao" class="brand-logo left"><i class="material-icons">dashboard</i></router-link>
+            <ul>
+              <li><router-link to="/comissao">Comissão</router-link></li>
+              <li><router-link to="/deputado">Deputados</router-link></li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
@@ -33,5 +35,9 @@ nav {
 nav .brand-logo {
   color: #1de9c3;
   font-weight: 200;
+}
+
+.menu-mobile ul {
+  margin-left: 40px;
 }
 </style>
